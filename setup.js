8bba566c87252d89d81829a0e4d6270b314c71fa
maxscha <maxscha@mail.de>
2021-01-30 11:35:39 +0100

    Adds proper parsing of timestamp

---
+++ setup.js
@@ -0,0 +1,3 @@
+//TODO Get all Scheduled messages from Token
+
+//TODO Delete all scheduled messages from token
--- src/message-builder.js
+++ src/message-builder.js
@@ -1,7 +1,7 @@
 const buildMessage = (channel, text, time) => {
     // TODO Think about channel resolution later. Would be nice if someone could just schedule based on channel-name
 
-    return {channel: channel, text: text, post_at: Date.parse(time)}
+    return {channel: channel, text: text, post_at: Date.parse(time)/1000}
 }
 
 module.exports = buildMessage;
