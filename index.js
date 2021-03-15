00a501535471b9151f6469eda322ac1e1453eae7
maxscha <maxscha@mail.de>
2021-03-15 21:25:23 +0100

    Adds checking of messages before deleting old scheduled messages

--- index.js
+++ index.js
@@ -27,25 +27,64 @@ function parseUserTokens(input) {
     }
 }
 
+function checkField(message, key) {
+    if (!message[key]) {
+        throw `${key} is missing in a message in ${message.file}`
+    }
+}
+
+function areMessagesCorrect(messages, userChannels, users) {
+    for (let message of messages) {
+        // TODO FOR EACH MESSAGE
+        // CHECK IF FIELDS EXIST TEXT, CHANNEL, ITEM
+        checkField(message, "text")
+        checkField(message, "post_at")
+        checkField(message, "channel")
+
+        try {
+            Date.parse(message.post_at)
+        } catch (e) {
+            throw `${e} in file ${message.file}`;
+        }
+        users = users.concat(["default"])
+        // CHECK IF HAS USER || DEFAULT AND IF USER EXISTS
+        const user = message.user || "default";
+        if (!users.includes(user)) {
+            throw `${user} is not found in token list in a message in file ${message.file}`
+        }
+        const channels = userChannels[user];
+        // TODO Implement proper handling of markdown to slack-markdown https://github.com/jsarafajr/slackify-markdown
+        if (convertChannelNameToId(message.channel, channels) == null) {
+            throw "Channel not found for message";
+        }
+
+
+
+    }
+}
+
 async function main() {
     //todo make proper async
     try {
         const messageFilePaths = core.getInput("message-file").split(";");
         const messages = [];
         for (let messageFilePath of messageFilePaths) {
-            messages.push(...yaml.load(fs.readFileSync(messageFilePath, 'utf8')));
+            const fileMessages = yaml.load(fs.readFileSync(messageFilePath, 'utf8'))
+            for (let fMessage of fileMessages) {
+                fMessage["file"] = messageFilePath;
+            }
+            messages.push(...fileMessages);
         }
 
 
         const userTokens = parseUserTokens(core.getInput('slack-user-oauth-access-token'));
-
-
-        // TODO check first if everything is defined properly (Every channel is properly specified, every user is properly specified and has permissions)
-        setup.deleteAllScheduledMessages(userTokens);
-
         const userChannels = await slack.getChannelsFromUser(userTokens);
 
 
+        areMessagesCorrect(messages, userChannels, Object.keys(userTokens));
+
+        // TODO check first if everything is defined properly (Every channel is properly specified, every user is properly specified and has permissions)
+        setup.deleteAllScheduledMessages(userTokens);
 
         for (let message of messages) {
             const user = message.user || "default";
