5e49ae710d88bf73b0e9412baac90952c231875c
maxscha <maxscha@mail.de>
2021-03-29 18:40:32 +0200

    Includes output of results

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const setup = require('./setup')
 
 
 
-
+// TODO Optimize api calls to avoid rate limits. E.g. don't delete messages which we would identically schedule again
+// TODO Add files: 1. Upload file, 2. Make file sharable 3. generte image text based on: https://stackoverflow.com/questions/58186399/how-to-create-a-slack-message-containing-an-uploaded-image/58189401#58189401
 
 async function main() {
     //TODO make proper async
@@ -27,15 +28,33 @@ async function main() {
 
         setup.deleteAllScheduledMessages(userTokens);
 
+        const results = [];
+
         for (let message of messages) {
             const user = message.user || "default";
             const channels = userChannels[user];
             const token = userTokens[user];
             const messageBuilded = buildMessage(convertChannelNameToId(message.channel, channels), message.text, message.post_at);
             const result = slack.sendMessage(token, messageBuilded);
+            result.catch((error) => {
+                console.error(`${error} for mesage: \n ${message.text}`)
+            });
+            results.push(result);
+
             //TODO put in proper error handling
         }
         // TODO Output scheduled messages        
+
+        for (let result of results) {
+            try {
+            
+                let r = await result;
+                console.log(r);
+            } catch (error) {
+                // console.error(error);
+            }
+        }
+
 
     } catch (error) {
         core.setFailed(error);
