6275d59eac4b6c436f6ba6ede25ccf337477c0be
ostend <smennear.nccu@gmail.com>
2021-08-19 19:05:36 -0400

    weekly repeating logic to schedule repeating posts

--- src/message-builder.js
+++ src/message-builder.js
@@ -21,16 +21,62 @@ function convertChannelNameToId(channel, channels) {
     }
     return null;
 }
+//* Repeats weekly (+ 7 ). 
+function incrimentDateIterative(dateObj, endDate) {
+    
+    dateObj = new Date(dateObj) 
+    endDate = new Date(endDate)
+    let dateList = []
+    let date
+   
+    while (dateObj !== endDate && dateObj < endDate) {     
+      date = new Date(dateObj.setDate(dateObj.getDate() + 7));      
+      dateList.push(date)
+        
+    } 
+    return dateList;
+  }
+  
+function buildRepeatMessages(messages){
+allMessages = []
 
+for (let i=0; i<messages.length; i++){
+    //* added an optional property 'repeat' to the .yaml objects
+    if(messages[i].repeat){
+        const dates = incrimentDateIterative(messages[i].post_at, messages[i].repeat);
+        //TODO figure out how to modify indv object properties, instead of rebuilding it
+        //! problem: modyfying indv object changed *all* object properties to be uniform
+        for(let j=0; j<dates.length; j++){
+            allMessages.push({
+                channel: messages[i].channel,
+                file:messages[i].file,
+                post_at:dates[j],    
+                text:messages[i].text,
+                user:messages[i].user,
+            })
+        }
+        
+    } if(!messages[i].repeat){
+        allMessages.push(messages[i])
+    }
+    
+
+
+    
+}
+
+return allMessages
+} 
 
 function areMessagesCorrect(messages, userChannels, users) {
     users = Object.keys(users).concat(["default"])
 
+    messages = buildRepeatMessages(messages)
     for (let message of messages) {
         checkField(message, "text")
         checkField(message, "post_at")
         checkField(message, "channel")
-
+        
         try {
             Date.parse(message.post_at)
         } catch (e) {
