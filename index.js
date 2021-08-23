622db000f0b7090a2909c516b1c74ded8b71efb8
Maximilian Schall <Maxscha@users.noreply.github.com>
2021-08-23 21:24:22 +0200

    Adds recurring messages (#7)
    * weekly repeating logic to schedule repeating posts
    
    * change variable names
    
    * refractor incrimentDateIterative() #4
    
    * fix if..else loop #4
    
    * conver the string date to Date object with function #4
    
    * fixed repeating message logic to include origional start date #4
    
    * fix ranges of collecting time period for repeats #4
    
    * moved buildRepeatMessages() to index.js so updated list of messages gets sent through to results
    
    * moved buildRepeatMessages() to index.js so updated list of messages gets sent through to results #4
    
    * delete console.log, buildRepeatMessages now in index.js #4
    
    * added const allMessages #4
    
    * check messges validity before building repeat messages #4
    
    Co-authored-by: ostend <smennear.nccu@gmail.com>

--- index.js
+++ index.js
@@ -1,68 +1,82 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { buildMessage, areMessagesCorrect, convertChannelNameToId } = require('./src/message-builder')
-const { parseUserTokens, checkUserTokens, parseMessageFileInput, loadMessage } = require('./src/input')
-const slack = require('./src/slack')
-const setup = require('./setup')
-
-
+const core = require("@actions/core");
+const github = require("@actions/github");
+const {
+  buildMessage,
+  areMessagesCorrect,
+  convertChannelNameToId,
+  buildRepeatMessages,
+} = require("./src/message-builder");
+const {
+  parseUserTokens,
+  checkUserTokens,
+  parseMessageFileInput,
+  loadMessage,
+} = require("./src/input");
+const slack = require("./src/slack");
+const setup = require("./setup");
 
 // TODO Optimize api calls to avoid rate limits. E.g. don't delete messages which we would identically schedule again
 // TODO Add files: 1. Upload file, 2. Make file sharable 3. generte image text based on: https://stackoverflow.com/questions/58186399/how-to-create-a-slack-message-containing-an-uploaded-image/58189401#58189401
 
 async function main() {
-    //TODO make proper async
-    try {
-        const messageFilePaths = parseMessageFileInput(core.getInput("message-file"));
+  //TODO make proper async
+  try {
+    const messageFilePaths = parseMessageFileInput(
+      core.getInput("message-file")
+    );
 
-        console.log(`${messageFilePaths.length} files found`);
-        const messages = loadMessage(messageFilePaths);
+    console.log(`${messageFilePaths.length} files found`);
+    const messages = loadMessage(messageFilePaths);
 
-        const userTokens = parseUserTokens(core.getInput('slack-user-oauth-access-token'));
+    const userTokens = parseUserTokens(
+      core.getInput("slack-user-oauth-access-token")
+    );
 
-        const workspaceId = core.getInput('slack-workspace-id');
-        const workspaceIdConverted = workspaceId === '' ? null : workspaceId;
-        checkUserTokens(userTokens, workspaceIdConverted)
+    const workspaceId = core.getInput("slack-workspace-id");
+    const workspaceIdConverted = workspaceId === "" ? null : workspaceId;
+    checkUserTokens(userTokens, workspaceIdConverted);
 
-        console.log(`${Object.keys(userTokens)} found as user`);
-        const userChannels = await slack.getChannelsFromUser(userTokens);
+    console.log(`${Object.keys(userTokens)} found as user`);
+    const userChannels = await slack.getChannelsFromUser(userTokens);
 
-        //TODO Change from implizit fail to explizit fail by collecting all failures and printing them before existing
-        areMessagesCorrect(messages, userChannels, userTokens);
+    //TODO Change from implizit fail to explizit fail by collecting all failures and printing them before existing
+    areMessagesCorrect(messages, userChannels, userTokens);
+    const allMessages = buildRepeatMessages(messages);
 
-        setup.deleteAllScheduledMessages(userTokens);
+    setup.deleteAllScheduledMessages(userTokens);
 
-        const results = [];
+    const results = [];
 
-        for (let message of messages) {
-            const user = message.user || "default";
-            const channels = userChannels[user];
-            const token = userTokens[user];
-            const messageBuilded = buildMessage(convertChannelNameToId(message.channel, channels), message.text, message.post_at);
-            const result = slack.sendMessage(token, messageBuilded);
-            result.catch((error) => {
-                console.error(`${error} for mesage: \n ${message.text}`)
-            });
-            results.push(result);
+    for (let message of allMessages) {
+      const user = message.user || "default";
+      const channels = userChannels[user];
+      const token = userTokens[user];
+      const messageBuilded = buildMessage(
+        convertChannelNameToId(message.channel, channels),
+        message.text,
+        message.post_at
+      );
+      const result = slack.sendMessage(token, messageBuilded);
+      result.catch((error) => {
+        console.error(`${error} for mesage: \n ${message.text}`);
+      });
+      results.push(result);
 
-            //TODO put in proper error handling
-        }
-        // TODO Output scheduled messages        
-
-        for (let result of results) {
-            try {
-
-                let r = await result;
-                console.log(r);
-            } catch (error) {
-                // console.error(error);
-            }
-        }
-
-
-    } catch (error) {
-        core.setFailed(error);
+      //TODO put in proper error handling
     }
+    // TODO Output scheduled messages
+
+    for (let result of results) {
+      try {
+        let r = await result;
+        console.log(r);
+      } catch (error) {
+        // console.error(error);
+      }
+    }
+  } catch (error) {
+    core.setFailed(error);
+  }
 }
 
 main();
--- src/message-builder.js
+++ src/message-builder.js
@@ -1,53 +1,114 @@
-const slackifyMarkdown = require('slackify-markdown');
+const slackifyMarkdown = require("slackify-markdown");
 
 const buildMessage = (channel, text, time) => {
-    return { channel: channel, text: slackifyMarkdown(text), post_at: Date.parse(time) / 1000 }
-}
+  return {
+    channel: channel,
+    text: slackifyMarkdown(text),
+    post_at: Date.parse(time) / 1000,
+  };
+};
 
 function checkField(message, key) {
-    if (!message[key]) {
-        throw `${key} is missing in a message in ${message.file}`
+  if (!message[key]) {
+    throw `${key} is missing in a message in ${message.file}`;
+  }
+}
+function stringToDateConverter(messages) {
+  for (let message of messages) {
+    message.post_at = new Date(message.post_at);
+    if (message.repeat) {
+      message.repeat = new Date(message.repeat);
     }
+  }
+  return messages;
 }
 
 function convertChannelNameToId(channel, channels) {
-    for (let c of channels) {
-        if (c.id == channel) {
-            return channel;
-        }
-        if (c.name == channel) {
-            return c.id;
-        }
+  for (let c of channels) {
+    if (c.id == channel) {
+      return channel;
     }
-    return null;
+    if (c.name == channel) {
+      return c.id;
+    }
+  }
+  return null;
 }
 
+function incrimentDateIterative(startDate, endDate) {
+  let date = new Date(startDate);
+  let dateList = [];
+
+  while (date < endDate) {
+    dateList.push(date);
+    date = new Date(startDate.setDate(startDate.getDate() + 7));
+  }
+
+  return dateList;
+}
+
+function buildRepeatMessages(messages) {
+  allMessages = [];
+
+  for (let i = 0; i < messages.length; i++) {
+    if (messages[i].repeat) {
+      const dates = incrimentDateIterative(
+        messages[i].post_at,
+        messages[i].repeat
+      );
+
+      //*This also gets rid of 'repeat' proterty
+      for (let j = 0; j < dates.length; j++) {
+        allMessages.push({
+          text: messages[i].text,
+          post_at: dates[j],
+          channel: messages[i].channel,
+          user: messages[i].user,
+          file: messages[i].file,
+        });
+      }
+    } else {
+      allMessages.push(messages[i]);
+    }
+  }
+
+  return allMessages;
+}
 
 function areMessagesCorrect(messages, userChannels, users) {
-    users = Object.keys(users).concat(["default"])
+  users = Object.keys(users).concat(["default"]);
 
-    for (let message of messages) {
-        checkField(message, "text")
-        checkField(message, "post_at")
-        checkField(message, "channel")
+  for (let message of messages) {
+    checkField(message, "text");
+    checkField(message, "post_at");
+    checkField(message, "channel");
 
-        try {
-            Date.parse(message.post_at)
-        } catch (e) {
-            throw `${e} in file ${message.file}`;
-        }
-
-        // CHECK IF HAS USER || DEFAULT AND IF USER EXISTS        
-        const user = message.user || "default";
-        if (!users.includes(user)) {
-            throw `${user} is not found in token list in a message in file ${message.file}`
-        }
-        const channels = userChannels[user];
-
-        if (convertChannelNameToId(message.channel, channels) == null) {
-            throw `Channel ${message.channel} not found for message in file ${message.file}`;
-        }
+    //TODO: throw error for NaN date
+    try {
+      Date.parse(message.post_at);
+      Date.parse(message.repeat);
+    } catch (e) {
+      throw `${e} in file ${message.file}`;
     }
+
+    // CHECK IF HAS USER || DEFAULT AND IF USER EXISTS
+    const user = message.user || "default";
+    if (!users.includes(user)) {
+      throw `${user} is not found in token list in a message in file ${message.file}`;
+    }
+    const channels = userChannels[user];
+
+    if (convertChannelNameToId(message.channel, channels) == null) {
+      throw `Channel ${message.channel} not found for message in file ${message.file}`;
+    }
+  }
+
+  messages = stringToDateConverter(messages);
 }
 
-module.exports = { buildMessage, areMessagesCorrect, convertChannelNameToId };
+module.exports = {
+  buildMessage,
+  areMessagesCorrect,
+  convertChannelNameToId,
+  buildRepeatMessages,
+};
